49ab5a4b3231056e5388dfe2d2c7939d5156e8be
omar kader <omarkader701@gmail.com>
2025-01-30 20:42:10 +0100

    Implemented Account class with deposit, withdraw, and printStatement methods

---
+++ src/main/java/com/omar/domain/Account.java
@@ -0,0 +1,25 @@
+package com.omar.domain;
+
+public class Account {
+
+    private final TransactionRepository transactionRepository;
+    private final StatementPrinter statementPrinter;
+
+    public Account(TransactionRepository transactionRepository, StatementPrinter statementPrinter) {
+        this.transactionRepository = transactionRepository;
+        this.statementPrinter = statementPrinter;
+    }
+
+    public void deposit(int amount) {
+        transactionRepository.addTransaction(amount);
+    }
+
+    public void withdraw(int amount) {
+        transactionRepository.addTransaction(-amount);
+    }
+
+    public void printStatement() {
+        List<Transaction> transactions = transactionRepository.getAllTransactions();
+        statementPrinter.print(transactions);
+    }
+}
---
+++ src/test/java/com/bank/infrastructure/StatementPrinterTest.java
@@ -0,0 +1,4 @@
+package com.bank.infrastructure;
+
+public class StatementPrinterTest {
+}
